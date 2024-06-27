005d686cf7273e9dc9804e953b1d4119bf8b0c23
phuc <phuct6776@gmail.com>
2024-06-28 00:32:15 +0700

    update data

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Successfully connnected to mongoDB 👍`);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Successfully connected to MongoDB 👍`);
   } catch (error) {
     console.error(`ERROR: ${error.message}`);
     process.exit(1);
